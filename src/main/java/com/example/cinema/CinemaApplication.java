18ccfdbdda1ac7e52c9c4114b66ea02264cba93d
sadiki-dev <sadiki.dev@gmail.com>
2021-05-19 12:32:36 +0000

    final project

---
+++ src/main/java/com/example/cinema/CinemaApplication.java
@@ -0,0 +1,39 @@
+package com.example.cinema;
+
+import com.example.cinema.entities.*;
+import com.example.cinema.service.CinemaInitServiceImpl;
+import com.example.cinema.service.ICinemaInitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CinemaApplication implements CommandLineRunner {
+
+    @Autowired
+    private ICinemaInitService cinemaInitService;
+    @Autowired
+    private RepositoryRestConfiguration restConfiguration;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        cinemaInitService.initVilles();
+        cinemaInitService.initCinemas();
+        cinemaInitService.initSalles();
+        cinemaInitService.initPlaces();
+        cinemaInitService.initSeances();
+        cinemaInitService.initCategories();
+        cinemaInitService.initFilms();
+        cinemaInitService.initProjections();
+        cinemaInitService.initTickets();
+
+        restConfiguration.exposeIdsFor(Cinema.class, Projection.class, Salle.class, Film.class, Ticket.class);
+
+    }
+}
---
+++ src/main/java/com/example/cinema/dao/ProjectionRepository.java
@@ -0,0 +1,13 @@
+package com.example.cinema.dao;
+
+import com.example.cinema.entities.Projection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+
+public interface ProjectionRepository extends JpaRepository<Projection,Long> {
+
+}
